8079c4c22a3aa6e7b3b4b046f157d20058c3ecb0
Keln <EgorKeln@narod.ru>
2025-03-28 23:10:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-function start() {
-    hideIntro();
-    // Здесь можно добавить логику для перехода на другую страницу или выполнения действий
+function assembling() {
+    hideIntro(() => {
+        window.location.href = 'assembling.html'; // Перенаправление на assembling.html
+    });
 }
 
-function learnMore() {
-    // Логика для перехода на страницу с подробной информацией
-    hideIntro();
-    // alert("Переход на страницу с подробной информацией...");
+function production() {
+    hideIntro(() => {
+        window.location.href = 'production.html'; // Перенаправление на production.html
+    });
 }
 
-function hideIntro() {
+function main() {
+    console.log("Функция main() вызвана"); // Сообщение для отладки
+    hideIntro(() => {
+        console.log("Перенаправление на index.html"); // Сообщение перед редиректом
+        window.location.href = 'index.html'; // Перенаправление на index.html
+    });
+}
+
+function hideIntro(callback) {
     const intro = document.getElementById('intro');
     intro.style.opacity = 0;
     setTimeout(() => {
         intro.style.display = 'none';
-    }, 1000); // 1 секунда на исчезновение
+        if (callback) callback(); // Выполнение колбэка после скрытия
+    }, 500); // 1 секунда на исчезновение
 }
-
-// Время срабатывания - через 10 секунд после появления заставки
-// setTimeout(hideIntro, 10000);
